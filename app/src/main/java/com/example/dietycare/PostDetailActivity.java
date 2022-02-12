1413b7d1b6237b39fe20156244da2055eeccd939
LizhiyuanLin <33528158+LizhiyuanLin@users.noreply.github.com>
2022-02-12 10:30:11 -0500

    add add comment

--- app/src/main/java/com/example/dietycare/PostDetailActivity.java
+++ app/src/main/java/com/example/dietycare/PostDetailActivity.java
@@ -1,9 +1,13 @@
 package com.example.dietycare;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -12,9 +16,8 @@ import androidx.appcompat.app.AppCompatActivity;
 public class PostDetailActivity extends AppCompatActivity {
 
     private Button record_bt, more_bt;
-    private ImageButton back_bt;
-    private TextView food_name_tv,food_info_tv;
-    private TextView cal_val_tv,pro_val_tv,fat_val_tv,carbo_val_tv;
+    private ImageButton back_bt, comment_bt;
+    private String commentText = "";
 
 
     @Override
@@ -36,18 +39,49 @@ public class PostDetailActivity extends AppCompatActivity {
                                    }
         );
 
-        /*//More Button
-        more_bt = (Button) findViewById(R.id.moreButton);
-        more_bt.setOnClickListener(new View.OnClickListener() {
+        //comment Button
+        comment_bt = findViewById(R.id.commentButton);
+        comment_bt.setOnClickListener(new View.OnClickListener() {
                                        @Override
                                        public void onClick(View view) {
-                                           Intent intent = new Intent(FoodDetailActivity.this, MoreNutrientActivity.class);
-                                           //Starting of the Intent
-                                           startActivity(intent);
-                                           finish();
+                                           AlertDialog.Builder builder = new AlertDialog.Builder(PostDetailActivity.this);
+                                           builder.setTitle("Comment This Post");
+
+// Set up the input
+                                           final EditText input = new EditText(PostDetailActivity.this);
+// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
+                                           input.setInputType(InputType.TYPE_CLASS_TEXT);
+                                           builder.setView(input);
+
+// Set up the buttons
+                                           builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                               @Override
+                                               public void onClick(DialogInterface dialog, int which) {
+                                                   commentText = input.getText().toString();
+                                               }
+                                           });
+                                           builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                                               @Override
+                                               public void onClick(DialogInterface dialog, int which) {
+                                                   dialog.cancel();
+                                               }
+                                           });
+
+                                           builder.show();
+
                                        }
                                    }
-        );*/
+        );
+
+        //set Post Owner's Name
+        TextView postOwnerName = findViewById(R.id.postOwnerName);
+        postOwnerName.setText("Amy");
+        //set Post content
+        TextView postContent = findViewById(R.id.postContent);
+        postContent.setText("Amysdfasdfas sef asAmysdfasdfas sef asfsdafsdoiafjsj dsaf sda fsdfadsfawgwegdfgdafg");
+        //set Post content
+        TextView postTime = findViewById(R.id.postTime);
+        postTime.setText("10:30 AM");
 
     }
 
