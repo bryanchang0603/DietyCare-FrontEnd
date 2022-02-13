aa906d4b7e02a55ef24e6236bccd43489fad662b
LizhiyuanLin <33528158+LizhiyuanLin@users.noreply.github.com>
2022-02-12 20:26:29 -0500

    got the framework ready

--- app/src/main/java/com/example/dietycare/PostDetailActivity.java
+++ app/src/main/java/com/example/dietycare/PostDetailActivity.java
@@ -1,21 +1,27 @@
 package com.example.dietycare;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class PostDetailActivity extends AppCompatActivity {
 
-    private Button record_bt, more_bt;
     private ImageButton back_bt, comment_bt;
     private String commentText = "";
 
@@ -27,7 +33,7 @@ public class PostDetailActivity extends AppCompatActivity {
         setContentView(R.layout.post_detail);
 
         //Back Button
-        back_bt = (ImageButton) findViewById(R.id.icBack);
+        back_bt = findViewById(R.id.icBack);
         back_bt.setOnClickListener(new View.OnClickListener() {
                                        @Override
                                        public void onClick(View view) {
@@ -47,13 +53,10 @@ public class PostDetailActivity extends AppCompatActivity {
                                            AlertDialog.Builder builder = new AlertDialog.Builder(PostDetailActivity.this);
                                            builder.setTitle("Comment This Post");
 
-// Set up the input
                                            final EditText input = new EditText(PostDetailActivity.this);
-// Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
                                            input.setInputType(InputType.TYPE_CLASS_TEXT);
                                            builder.setView(input);
 
-// Set up the buttons
                                            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                                @Override
                                                public void onClick(DialogInterface dialog, int which) {
@@ -78,30 +81,46 @@ public class PostDetailActivity extends AppCompatActivity {
         postOwnerName.setText("Amy");
         //set Post content
         TextView postContent = findViewById(R.id.postContent);
-        postContent.setText("Amysdfasdfas sef asAmysdfasdfas sef asfsdafsdoiafjsj dsaf sda fsdfadsfawgwegdfgdafg");
+        postContent.setText("Amysdfasdfas sef");
         //set Post content
         TextView postTime = findViewById(R.id.postTime);
         postTime.setText("10:30 AM");
 
-    }
+        //set Comments list
+        ListView listview = findViewById(R.id.commentList);
+        ArrayList<String> list = new ArrayList<String>();
+        list.add("Jason: I love youI love youI love youI love youI love you");
+        list.add("2: I love yoI love youI love youI love youu");
+        list.add("3: I love yoI love youI love youu");
+        list.add("4: I love youI love youI love youI love you");
+        list.add("5: I love youI love youI love youI love you");
+        StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
 
-
-    private String get_food_name() {
-        return "Apple";
     }
-    private String get_food_info (){
-        return "Apple is 11.5 kcal.";
-    };
-    private String get_cal (){
-        return "11.5kcal";
-    };//Double to String;
-    private String get_pro (){
-        return "50g";
-    };
-    private String get_fat (){
-        return "40g";
-    };
-    private String get_carbo (){
-        return "60g";
-    };
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
+    }
 }
